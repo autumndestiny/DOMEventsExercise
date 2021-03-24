3ec0a7214a3e540b4cfe0ea075e8f76da39f4a1c
Autumn Richardson <autumnrichardson90@gmail.com>
2021-03-24 09:18:06 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// 1a
+const button1 = document.querySelector(`#one`);
+
+// 1b
+button1.onclick = () => alert("Cool! You can hear/ understadn a hummingbird and ant.");
+
+// 2a
+const h3 = document.querySelector(`h3`);
+
+// 2b
+h3.addEventListener(`mouseover`, () => alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one."));
+
+// 3a
+const form = document.querySelector(`form`);
+
+// 3b
+form.addEventListener(`submit`, q => {
+    q.preventDefault();
+    const mess = form.elements.quote.value;
+    alert(`${mess}`);
+  });
+
+// 4a
+const darkMode = document.querySelector(`#dm`);
+
+// 4b
+darkMode.addEventListener(`click`, () => {
+    for (element of document.querySelectorAll(`*`)){
+        element.classList.toggle(`dark-mode`);
+    }
+});
+
+// 5a
+const reality = document.querySelector(`#reality`);
+
+// 5b
+let i = 1;
+function realityJump(){
+  if (i < 3){
+    alert("You have successfully moved to another reality");
+    i++;
+  } else {
+    alert("OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!");
+    reality.removeEventListener(`click`, realityJump);
+  }
+}
+reality.addEventListener(`click`, realityJump);
